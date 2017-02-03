63a7b3e9873fe163cfec9cf9378811545b5fb48a
pavelsof <pavelsof@gmail.com>
2017-02-03 15:27:50 +0100

    Chu-Liu/Edmonds' algorithm seems to be operational.

--- code/chu_liu.py
+++ code/chu_liu.py
@@ -27,11 +27,26 @@ class Graph:
 		self.scores = scores
 	
 	
+	def __str__(self):
+		"""
+		Returns a string with the graph's nodes, edges and scores. Useful for
+		debugging.
+		"""
+		def set_to_str(s):
+			return ', '.join(map(str, sorted(s)))
+		
+		return '\n'.join([
+			'nodes: {}'.format(set_to_str(self.nodes)),
+			'edges: {}'.format(set_to_str(self.edges)),
+			'scores: {}'.format(', '.join(['{}: {}'.format(key, value)
+				for key, value in sorted(self.scores.items())]))])
+	
+	
 	def create_greedy_edges(self):
 		"""
 		For each node other than the root, it adds the maximum-score
-		child-parent link. Errors from self.scores not containing an edge are
-		left propagate.
+		child-parent link. Raises a ValueError if no incoming edge can be added
+		for any of the non-root nodes.
 		
 		This method mutates the instance. If the graph already has some edges,
 		these are discarded.
@@ -39,16 +54,21 @@ class Graph:
 		self.edges = set()
 		
 		for j in filter(lambda x: x != 0, self.nodes):
-			max_score = 0
+			max_score = -1
 			parent = None
 			
 			for i in filter(lambda x: x != j, self.nodes):
+				if (i, j) not in self.scores:
+					continue
+				
 				if self.scores[(i, j)] > max_score:
 					max_score = self.scores[(i, j)]
 					parent = i
 			
-			if parent is not None:
-				self.edges.add((parent, j))
+			if parent is None:
+				raise ValueError('No incoming edge for node {}'.format(j))
+			
+			self.edges.add((parent, j))
 	
 	
 	def find_cycle(self):
@@ -83,48 +103,73 @@ class Graph:
 		cycle = self.find_cycle()
 		
 		new_nodes = (self.nodes - cycle) | set([c_node])
-		new_edges = set()
-		new_scores = {
-			edge: score for edge, score in self.scores.items()
-			if edge[0] not in cycle and edge[1] not in cycle}
+		new_scores = {}
 		
-		for edge in self.edges:
+		for edge, score in self.scores.items():
 			if edge[0] in cycle and edge[1] in cycle:
-				pass
+				continue
 			
 			elif edge[0] in cycle:
 				new_scores[(c_node, edge[1])] = max([
 					score for edge_, score in self.scores.items()
 					if edge_[0] in cycle and edge_[1] == edge[1]])
-				new_edges.add((c_node, edge[1]))
 			
 			elif edge[1] in cycle:
 				new_scores[(edge[0], c_node)] = max([
 					score for edge_, score in self.scores.items()
 					if edge_[0] == edge[0] and edge_[1] in cycle])
-				new_edges.add((edge[0], c_node))
 			
 			else:
-				new_edges.add(edge)
+				new_scores[edge] = score
 		
-		return cycle, Graph(new_nodes, new_edges, new_scores)
+		assert all([edge[0] in new_nodes and edge[1] in new_nodes
+			for edge in new_scores.keys()])
+		
+		return cycle, Graph(new_nodes, set(), new_scores)
 	
 	
-	def expand(self, original_graph, cycle, c_node):
+	def expand(self, orig_graph, cycle, c_node):
 		"""
 		Returns a new Graph instance with the specified node expanded back into
 		the nodes that had been contracted in order to create it.
 		
 		This method does not mutate the instance.
 		"""
-		parent = filter(lambda edge: edge[1] == c_node, self.edges)[0]
+		new_edges = set()
+		for edge in self.edges:
+			if edge[0] == c_node:
+				new_edges.add(next((
+					arc for arc, score in orig_graph.scores.items()
+					if arc[1] == edge[1] and arc[0] in cycle
+					and score == self.scores[edge])))
+			elif edge[1] == c_node:
+				new_edges.add(next((
+					arc for arc, score in orig_graph.scores.items()
+					if arc[0] == edge[0] and arc[1] in cycle
+					and score == self.scores[edge])))
+			else:
+				new_edges.add(edge)
 		
-		new_nodes = (self.nodes - set([c_node])) | cycle
-		new_edges = set(self.edges)
+		cycle_parent = next((
+			edge[0] for edge in self.edges if edge[1] == c_node))
+		edge_to_del = next((
+			edge for edge in orig_graph.edges
+			if edge[0] in cycle and edge[1] in cycle
+			and (cycle_parent, edge[1]) in new_edges))
+		
+		for edge in orig_graph.edges:
+			if edge[0] in cycle and edge[1] in cycle and edge != edge_to_del:
+				new_edges.add(edge)
+		
+		assert all([
+			edge[0] in orig_graph.nodes and edge[1] in orig_graph.nodes
+			for edge in new_edges])
+		
+		return Graph(orig_graph.nodes, new_edges, orig_graph.scores)
 
 
 
-def find(graph):
+def find_max_tree(graph):
 	"""
 	The main function of Chu-Liu/Edmond's algorithm. Expects a Graph instance
 	and a dictionary with the scores. The latter should have an entry for each
@@ -135,9 +180,22 @@ def find(graph):
 	"""
 	graph.create_greedy_edges()
 	
+	c_node = generate_node_name(graph.nodes)
+	
 	try:
-		new_graph, new_scores = graph.contract()
+		cycle, new_graph = graph.contract(c_node)
 	except ValueError:
 		return graph
 	
-	new_graph = find(new_graph, new_scores)
+	new_graph = find_max_tree(new_graph)
+	
+	return new_graph.expand(graph, cycle, c_node)
+
+
+
+def generate_node_name(nodes):
+	"""
+	Returns a fresh node name not present in the given set. Assumes that node
+	names are integers.
+	"""
+	return max(nodes) + 1
--- code/tests/test_chu_liu.py
+++ code/tests/test_chu_liu.py
@@ -1,10 +1,10 @@
 from unittest import TestCase
 
-from hypothesis.strategies import integers, lists
+from hypothesis.strategies import integers, lists, sets
 from hypothesis.strategies import composite
 from hypothesis import given
 
-from code.chu_liu import Graph
+from code.chu_liu import Graph, find_max_tree, generate_node_name
 
 
 
@@ -16,7 +16,7 @@ def square_matrices(draw, size):
 
 
 
-class GraphTestCase(TestCase):
+class ChuLiuTestCase(TestCase):
 	
 	@given(integers(min_value=1, max_value=10).flatmap(
 			lambda x: square_matrices(x)))
@@ -33,6 +33,10 @@ class GraphTestCase(TestCase):
 			self.assertEqual(
 				matrix[parent][child],
 				max([matrix[i][child] for i in range(size) if i != child]))
+		
+		# ensure each node apart from the root has a parent
+		for node in range(1, size):
+			self.assertTrue(any([edge[1] == node for edge in graph.edges]))
 	
 	
 	def test_find_cycle(self):
@@ -46,16 +50,69 @@ class GraphTestCase(TestCase):
 			graph.find_cycle()
 	
 	
-	def test_contract(self):
-		graph = Graph(range(4),
-			[(0, 1), (1, 2), (2, 1), (2, 3)],
-			{(0, 1): 1, (1, 2): 2, (2, 1): 3, (2, 3): 4})
+	@given(square_matrices(4))
+	def test_contract_and_expand(self, matrix):
+		scores = {(i, j): matrix[i][j] for i in range(4) for j in range(4)}
 		
-		cycle, new_graph = graph.contract('c')
+		graph1 = Graph(range(4), [(0, 1), (1, 2), (2, 1), (2, 3)], scores)
+		
+		cycle, graph2 = graph1.contract('c')
+		graph2.create_greedy_edges()
 		
 		self.assertEqual(cycle, set([1, 2]))
+		self.assertEqual(graph2.nodes, set([0, 'c', 3]))
+		self.assertFalse(any([
+			edge[0] in cycle or edge[1] in cycle
+			for edge in graph2.scores.keys()]))
 		
-		self.assertEqual(new_graph.nodes, set([0, 'c', 3]))
-		self.assertEqual(new_graph.edges, set([(0, 'c'), ('c', 3)]))
+		graph3 = graph2.expand(graph1, cycle, 'c')
+		self.assertEqual(graph3.nodes, set(range(4)))
+		self.assertEqual(graph3.scores, scores)
+	
+	
+	@given(square_matrices(5))
+	def test_contract_and_expand2(self, matrix):
+		scores = {(i, j): matrix[i][j] for i in range(5) for j in range(5)}
 		
-		self.assertEqual(new_graph.scores, {(0, 'c'): 1, ('c', 3): 4})
+		graph1 = Graph(range(5),
+			[(0, 1), (1, 2), (2, 3), (3, 1), (3, 4)], scores)
+		
+		cycle, graph2 = graph1.contract('c')
+		graph2.create_greedy_edges()
+		
+		self.assertEqual(cycle, set([1, 2, 3]))
+		self.assertEqual(graph2.nodes, set([0, 'c', 4]))
+		self.assertFalse(any([
+			edge[0] in cycle or edge[1] in cycle
+			for edge in graph2.scores.keys()]))
+		
+		graph3 = graph2.expand(graph1, cycle, 'c')
+		self.assertEqual(graph3.nodes, set(range(5)))
+		self.assertEqual(graph3.scores, scores)
+	
+	
+	@given(sets(integers(), min_size=1))
+	def test_generate_node_name(self, nodes):
+		new_node = generate_node_name(nodes)
+		self.assertEqual(len(nodes | set([new_node])), len(nodes)+1)
+	
+	
+	@given(integers(min_value=1, max_value=10).flatmap(
+			lambda x: square_matrices(x)))
+	def test_find_max_tree_does_not_break(self, matrix):
+		size = len(matrix)
+		scores = {
+			(i, j): matrix[i][j]
+			for i in range(size) for j in range(size)}
+		
+		graph = Graph(range(size), scores=scores)
+		
+		res = find_max_tree(graph)
+		
+		self.assertTrue(isinstance(res, Graph))
+		self.assertEqual(res.nodes, set(range(size)))
+		self.assertEqual(res.scores, scores)
+		
+		# ensure each node apart from the root has a parent
+		for node in range(1, size):
+			self.assertTrue(any([edge[1] == node for edge in res.edges]))
